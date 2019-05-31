d88cc35a1d64e2a2702d262f03ee2e55be0def60
Luiz Menezes <luiz.menezesf@gmail.com>
2019-05-30 22:40:24 -0300

    Improve homepage view to use ListView instead of TemplateView

--- podcast/views.py
+++ podcast/views.py
@@ -1,15 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
 from .models import Episode
 
 
-class HomeView(TemplateView):
+class HomeView(ListView):
     template_name = 'index.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-        episodes = Episode.objects.all().order_by('-publish_date')
-        if episodes:
-            context['last_episode'] = episodes[0]
-            context['episodes'] = episodes[1:]
-        return context
+    paginate_by = 10
+    queryset = Episode.objects.order_by('-publish_date')
+    context_object_name = 'episodes'
